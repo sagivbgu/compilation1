8bfd532b814d4229008738d69780fe1b39d884b0
Gil Heller <heller.gil@gmail.com>
2020-11-18 19:10:43 +0200

    fixed dotted list

--- list.ml
+++ list.ml
@@ -1,4 +1,4 @@
-#use "symbol.ml";;
+#use "number.ml";;
 
 (* 
   ⟨List⟩ ::= ( ⟨Sexpr⟩∗ )
@@ -14,6 +14,7 @@ let tok_list nt_sexpr =
   parenth_sexpr;;
 
 let tok_dotted_list nt_sexpr = 
+  let dot = char '.' in
   let p_sexpr = plus nt_sexpr in
   let dotted_sexpr = caten p_sexpr (caten dot nt_sexpr) in
   let parenth_dotted_sexpr = make_nt_parenthesized_expr dotted_sexpr in
@@ -21,26 +22,18 @@ let tok_dotted_list nt_sexpr =
     (function (l, (d, r)) -> l@[r]) in
   pack parenth_dotted_sexpr _remove_dot;;
 
-let rec list_to_pairs lst = 
+let rec list_to_pairs_end_with_nil lst = 
   match lst with
   | [] -> Nil
+  | a::rest -> Pair (a,(list_to_pairs_end_with_nil rest));;
+
+let rec list_to_pairs lst = 
+  match lst with
+  | a::[b] -> Pair (a, b)
   | a::rest -> Pair (a,(list_to_pairs rest));;
 
-let make nt_list nt_sexpr =
-  pack (tok_list nt_sexpr) list_to_pairs;;
+let make_nt_list nt_sexpr =
+  pack (tok_list nt_sexpr) list_to_pairs_end_with_nil;;
 
 let make_nt_dotted_list nt_sexpr = 
   pack (tok_dotted_list nt_sexpr) list_to_pairs;;
-
-(* TESTS *)
-
-tok_list (string_to_list "   (1 1    1)  ");;
-tok_dotted_list (string_to_list " ( 1 2 3 . 4) ");;
-
-Printf.printf " --- ";;
-
-nt_list (string_to_list "   (1 1    1)  ");;
-nt_list (string_to_list "   (   )  ");;
-
-nt_dotted_list (string_to_list " ( 1 2 3 . 4) ");;
-nt_dotted_list (string_to_list " ( 0.111 0.222 0.3333 . 0.4444) ");;
