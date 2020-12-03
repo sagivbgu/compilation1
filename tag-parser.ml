15db03cfcce2705e7d031ad5793ac17f1ea271b9
sagivbgu <sagivsc@post.bgu.ac.il>
2020-12-03 12:54:25 +0200

    Started implementing tag-parser

--- tag-parser.ml
+++ tag-parser.ml
@@ -22,26 +22,26 @@ let rec expr_eq e1 e2 =
   | Const(Sexpr s1), Const(Sexpr s2) -> sexpr_eq s1 s2
   | Var(v1), Var(v2) -> String.equal v1 v2
   | If(t1, th1, el1), If(t2, th2, el2) -> (expr_eq t1 t2) &&
-                                            (expr_eq th1 th2) &&
-                                              (expr_eq el1 el2)
+                                          (expr_eq th1 th2) &&
+                                          (expr_eq el1 el2)
   | (Seq(l1), Seq(l2)
     | Or(l1), Or(l2)) -> List.for_all2 expr_eq l1 l2
   | (Set(var1, val1), Set(var2, val2)
     | Def(var1, val1), Def(var2, val2)) -> (expr_eq var1 var2) &&
-                                             (expr_eq val1 val2)
+                                           (expr_eq val1 val2)
   | LambdaSimple(vars1, body1), LambdaSimple(vars2, body2) ->
-     (List.for_all2 String.equal vars1 vars2) &&
-       (expr_eq body1 body2)
+    (List.for_all2 String.equal vars1 vars2) &&
+    (expr_eq body1 body2)
   | LambdaOpt(vars1, var1, body1), LambdaOpt(vars2, var2, body2) ->
-     (String.equal var1 var2) &&
-       (List.for_all2 String.equal vars1 vars2) &&
-         (expr_eq body1 body2)
+    (String.equal var1 var2) &&
+    (List.for_all2 String.equal vars1 vars2) &&
+    (expr_eq body1 body2)
   | Applic(e1, args1), Applic(e2, args2) ->
-     (expr_eq e1 e2) &&
-       (List.for_all2 expr_eq args1 args2)
+    (expr_eq e1 e2) &&
+    (List.for_all2 expr_eq args1 args2)
   | _ -> false;;
-	
-                       
+
+
 exception X_syntax_error;;
 
 module type TAG_PARSER = sig
@@ -50,16 +50,42 @@ end;; (* signature TAG_PARSER *)
 
 module Tag_Parser : TAG_PARSER = struct
 
-let reserved_word_list =
-  ["and"; "begin"; "cond"; "define"; "else";
-   "if"; "lambda"; "let"; "let*"; "letrec"; "or";
-   "quasiquote"; "quote"; "set!"; "pset!"; "unquote";
-   "unquote-splicing"];;  
+  let reserved_word_list =
+    ["and"; "begin"; "cond"; "define"; "else";
+     "if"; "lambda"; "let"; "let*"; "letrec"; "or";
+     "quasiquote"; "quote"; "set!"; "pset!"; "unquote";
+     "unquote-splicing"];;  
 
-(* work on the tag parser starts here *)
+  (* work on the tag parser starts here *)
 
-let tag_parse_expressions sexpr = raise X_not_yet_implemented;;
+  (* *************** UTILS ***************** *)
+  let rec pairs_to_list = function
+    | Nil -> []
+    | Pair(first, second) -> first :: pairs_to_list second
 
-  
+  (* *************** EXPR ***************** *)
+  let rec tag_parse_expression = function
+    | Bool(b) -> Const(Sexpr(Bool(b)))
+    | Char(c) -> Const(Sexpr(Char(c)))
+    | Number(n) -> Const(Sexpr(Number(n)))
+    | String(s) -> Const(Sexpr(String(s)))
+    | Pair(Symbol "quote", Pair(sexpr, Nil)) -> Const(Sexpr(sexpr)) (* Intentionally not recursive *)
+    | Pair(Symbol "if", rest_of_if) ->  tag_parse_if (pairs_to_list rest_of_if)
+  and tag_parse_if = function
+    | [if_test; if_then; if_else] -> If((tag_parse_expression if_test),
+                                        (tag_parse_expression if_then),
+                                        (tag_parse_expression if_else)
+                                       )
+    | [if_test; if_then] -> If((tag_parse_expression if_test),
+                               (tag_parse_expression if_then),
+                               Const(Void)
+                              )
+    | _ -> raise X_syntax_error
+  ;;
+
+  (* *************** TAG-PARSER ***************** *)
+  let tag_parse_expressions sexpr = List.map tag_parse_expression sexpr;;
+
+
 end;; (* struct Tag_Parser *)
 
