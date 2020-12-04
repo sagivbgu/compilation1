61578ec175856cc0106ebb6b4364b4be971b2cf5
sagivbgu <sagivsc@post.bgu.ac.il>
2020-12-04 17:02:03 +0200

    United work of Person A & B

--- tag-parser.ml
+++ tag-parser.ml
@@ -58,6 +58,35 @@ module Tag_Parser : TAG_PARSER = struct
 
   (* work on the tag parser starts here *)
 
+  (* *************** UNREAD ***************** *)
+  let  unread_number n =
+    match n with
+    | Fraction(nom, denom) -> Printf.sprintf "%d/%d" nom denom
+    | Float(f) -> Printf.sprintf "%f" f
+
+  let unread_char c =
+    let scm_char_name = 
+      match c with
+      | '\n' -> "newline"
+      | '\r' -> "return"
+      | '\x00' -> "nul"
+      | '\x0c' -> "page"
+      | ' ' -> "space"
+      | '\t' -> "tab"
+      | _ -> String.make 1 c in
+    Printf.sprintf "#\\%s" scm_char_name
+
+  let rec unread s = 
+    match s with
+    | Bool(true) -> Printf.sprintf "#t"
+    | Bool(false) -> Printf.sprintf "#f"
+    | Nil -> Printf.sprintf "()"
+    | Number(n) -> unread_number n
+    | Char(c) -> unread_char c
+    | String(s) -> Printf.sprintf "\"%s\"" s
+    | Symbol(s) -> Printf.sprintf "%s" s
+    | Pair(car, cdr) -> Printf.sprintf "(%s . %s)" (unread car) (unread cdr);;
+
   (* *************** UTILS ***************** *)
   type 'a pairlist = 
     | ProperList of 'a list
@@ -88,25 +117,92 @@ module Tag_Parser : TAG_PARSER = struct
     | Number(n) -> Const(Sexpr(Number(n)))
     | String(s) -> Const(Sexpr(String(s)))
     | Pair (Symbol "quote", Pair(sexpr, Nil)) -> Const(Sexpr(sexpr)) (* Intentionally not recursive *)
-    | Pair (Symbol "if", rest_of_if) ->  tag_parse_if (pair_to_list_if_proper rest_of_if)
-    | Pair (Symbol "lambda", rest_of_lambda) ->  tag_parse_lambda (pair_to_list_if_proper rest_of_lambda)
-    | Pair (Symbol "define", rest_of_define) ->  tag_parse_define rest_of_define
-    | Pair (Symbol "let", rest_of_let) ->  tag_parse_let rest_of_let
-    | Pair (Symbol "let*", rest_of_let) ->  tag_parse_let_star rest_of_let
-    | Pair (Symbol "letrec", rest_of_let) ->  tag_parse_letrec rest_of_let
-    (* Application following should be after all other symbols *)
+    | Pair (Symbol "if", rest_of_if) -> tag_parse_if (pair_to_list_if_proper rest_of_if)
+    | Pair (Symbol "or", exps) -> tag_parse_or (pair_to_list_if_proper exps)
+    | Pair (Symbol "set!", exps) -> tag_parse_set (pair_to_list_if_proper exps)
+    | Pair (Symbol "begin", exps) -> tag_parse_seq (pair_to_list_if_proper exps)
+    | Pair (Symbol "and", exps) -> tag_parse_and exps
+    | Pair (Symbol "cond", exps) -> tag_parse_cond exps
+    | Pair (Symbol "lambda", rest_of_lambda) -> tag_parse_lambda (pair_to_list_if_proper rest_of_lambda)
+    | Pair (Symbol "define", rest_of_define) -> tag_parse_define rest_of_define
+    | Pair (Symbol "let", rest_of_let) -> tag_parse_let rest_of_let
+    | Pair (Symbol "let*", rest_of_let) -> tag_parse_let_star rest_of_let
+    | Pair (Symbol "letrec", rest_of_let) -> tag_parse_letrec rest_of_let
     | Pair (func, params) -> tag_parse_applic func (pair_to_list_if_proper params)
+    | Symbol(var) -> tag_parse_var var
+    | x -> Printf.printf "\n%s\n" (unread x); raise X_not_yet_implemented
 
   and tag_parse_if = function
     | [if_test; if_then; if_else] -> If((tag_parse_expression if_test),
-                                                    (tag_parse_expression if_then),
-                                                    (tag_parse_expression if_else)
-                                                   )
+                                        (tag_parse_expression if_then),
+                                        (tag_parse_expression if_else)
+                                       )
     | [if_test; if_then] -> If((tag_parse_expression if_test),
-                                           (tag_parse_expression if_then),
-                                           Const(Void)
-                                          )
+                               (tag_parse_expression if_then),
+                               Const(Void)
+                              )
     | _ -> raise X_syntax_error
+
+  and tag_parse_or = function
+    | [] -> Or([(tag_parse_expression (Bool(false)))])
+    | lst -> Or((List.map tag_parse_expression lst))
+
+  and tag_parse_set = function
+    | [exp1; exp2] -> Set ((tag_parse_expression exp1),(tag_parse_expression exp2))
+    | x -> raise X_syntax_error
+
+  (* Seq needs to be flatten -> Seq([x ; Seq(y)]) => Seq([x; y]) *)
+  and tag_parse_seq = function
+    | [] -> (Const Void)
+    | [sexp] -> (tag_parse_expression sexp)
+    | sexps -> Seq( (List.flatten (List.map flatten_seq (List.map tag_parse_expression sexps))) )
+
+  and flatten_seq = function
+    | Seq(x) -> x
+    | x -> [x]
+
+
+  and tag_parse_and = function
+    | Nil -> (tag_parse_expression (Bool(true)))
+    | Pair(first, Nil) -> (tag_parse_expression first)
+    | Pair(first, rest) -> (tag_parse_if [first ; Pair(Symbol("and"), rest) ; Bool(false)])
+    | _ -> raise X_syntax_error
+
+  and tag_parse_cond = function
+    | Pair(Pair(Symbol("else"), dit ), _) -> parse_cond_else_rib dit
+    | Pair(Pair(cond, Pair(Symbol("=>"), func)), next) -> parse_cond_rib_2 cond func next
+    | Pair(Pair(cond, dit), next) -> parse_cond_rib_1 cond dit next
+    | _ -> raise X_syntax_error
+
+  and parse_cond_rib_1 cond dit next = 
+    If((tag_parse_expression cond), 
+       (tag_parse_expression (Pair(Symbol("begin"), dit))), 
+       (tag_parse_cond next))
+
+  and parse_cond_rib_2 cond func next =
+    let expanded = 
+      Pair (Symbol "let",
+            Pair
+              (Pair (Pair (Symbol "value", Pair (cond, Nil)),
+                     Pair
+                       (Pair (Symbol "f",
+                              Pair (Pair (Symbol "lambda", Pair (Nil, Pair (func, Nil))),
+                                    Nil)),
+                        Pair
+                          (Pair (Symbol "rest",
+                                 Pair (Pair (Symbol "lambda", Pair (Nil, Pair (Pair(Symbol "cond", next), Nil))),
+                                       Nil)),
+                           Nil))),
+               Pair
+                 (Pair (Symbol "if",
+                        Pair (Symbol "value",
+                              Pair (Pair (Pair (Symbol "f", Nil), Pair (Symbol "value", Nil)),
+                                    Pair (Pair (Symbol "rest", Nil), Nil)))),
+                  Nil))) in
+    (tag_parse_expression expanded)
+
+  and parse_cond_else_rib dit = 
+    tag_parse_if [(Bool(true)); (Pair(Symbol("begin"), dit))]
 
   and tag_parse_lambda rest_of_lambda =
     let get_var = function Symbol(v) -> v | _ -> raise X_syntax_error
@@ -205,13 +301,11 @@ module Tag_Parser : TAG_PARSER = struct
 
     | _ -> raise X_syntax_error
 
+  and tag_parse_var var = 
+    if (List.mem var reserved_word_list)
+    then (raise X_syntax_error) 
+    else (Var(var))
 
-  and tag_parse_seq = function
-    (*
-    Dear implementor, pay attention to change call to this function from
-    tag_parse_lambda if needed (there's an implicit sequence inside lambda).
-    *)
-    | _ -> raise X_not_yet_implemented (* TODO *)
 
   and tag_parse_applic func params = Applic((tag_parse_expression func), (tag_parse_exprs params))
 
