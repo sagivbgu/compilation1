ddc8ff5b93b0a78d85d01ceac51efb2d07401b47
Gil Heller <heller.gil@gmail.com>
2021-01-04 15:16:55 +0200

    Closure - Done! (i hope...)

--- code-gen.ml
+++ code-gen.ml
@@ -271,6 +271,12 @@ module Code_Gen : CODE_GEN = struct
       let cmd = Printf.sprintf "mov rax, qword [fvar_tbl+8*%d]" index in
       let cmd_with_comment = Printf.sprintf "%s\t; mov fvar %s to rax" cmd fvar in
       cmd_with_comment in
+    
+    let get_lambda_depth =
+      let depth = ref (-1) in
+      fun () ->
+        incr depth;
+        !depth in
 
     let rec generate_exp consts fvars expr = match expr with
       | Const'(const) -> generate_const consts const
@@ -388,13 +394,10 @@ jmp " ^ end_if_label ^ "
     
     and generate_get_var_bound var_name major minor = 
       let operation_description = Printf.sprintf "Get VarBound(name=%s, major=%d, minor=%d)" var_name major minor in
-      let cmd_0 = Printf.sprintf "mov rax, qword [rbp + 8 * 2]" in
-      (* We need to pass the LIST_HEAD object 
-        [First qword is length | Second qword is the pointer to the majors rib] *)
-      let cmd_1 = Printf.sprintf "mov rax, qword [rax + 8]" in 
+      let cmd_1 = Printf.sprintf "mov rax, qword [rbp + 8 * 2]" in
       let cmd_2 = Printf.sprintf "mov rax, qword [rax + 8 * %d]" major in
       let cmd_3 = Printf.sprintf "mov rax, qword [rax + 8 * %d]" minor in
-      let cmd = cmd_0 ^ "\n" ^ cmd_1 ^ "\n" ^ cmd_2 ^ "\n" ^ cmd_3 in
+      let cmd = cmd_1 ^ "\n" ^ cmd_2 ^ "\n" ^ cmd_3 in
       let cmd_with_comment = get_commented_cmd_string operation_description cmd in
       cmd_with_comment
     
@@ -403,14 +406,11 @@ jmp " ^ end_if_label ^ "
       let rhs_operation_description = Printf.sprintf "Evaluating rhs [ %s ], value is expected in rax" operation_description in
       let rhs_cmd = generate_exp consts fvars rhs in
       let rhs_cmd_with_comment = get_commented_cmd_string rhs_operation_description rhs_cmd in
-      let assign_cmd_0 = Printf.sprintf "mov rbx, qword [rbp + 8 ∗ 2]" in
-      (* We need to pass the LIST_HEAD object 
-        [First qword is length | Second qword is the pointer to the majors rib] *)
-      let assign_cmd_1 = Printf.sprintf "mov rbx, qword [rbx + 8]" in 
+      let assign_cmd_1 = Printf.sprintf "mov rbx, qword [rbp + 8 ∗ 2]" in
       let assign_cmd_2 = Printf.sprintf "mov rbx, qword [rbx + 8 ∗ %d]" major in
       let assign_cmd_3 = Printf.sprintf "mov qword [rbx + 8 ∗ %d], rax" minor in
       let assign_cmd_4 = Printf.sprintf "mov rax, SOB_VOID_ADDRESS" in
-      let assign_cmd = "\n" ^ assign_cmd_0 ^ "\n" ^ assign_cmd_1 ^ "\n" ^ assign_cmd_2 ^ "\n" ^ assign_cmd_3 ^ "\n" ^ assign_cmd_4 in
+      let assign_cmd = "\n" ^ assign_cmd_1 ^ "\n" ^ assign_cmd_2 ^ "\n" ^ assign_cmd_3 ^ "\n" ^ assign_cmd_4 in
       let cmd = rhs_cmd_with_comment ^ assign_cmd in
       let cmd_with_comment = get_commented_cmd_string operation_description cmd in
       cmd_with_comment
@@ -444,15 +444,196 @@ jmp " ^ end_if_label ^ "
       (* Init *)
       let operation_index = get_operation_index() in  
       let operation_description = Printf.sprintf "Creating CLOSURE of LambdaSimple#%d -> %s" operation_index (untag (LambdaSimple'(p_names, body))) in 
-      
+      let depth = get_lambda_depth() in
       let lcont_label =  Printf.sprintf "LClosureCont%d" operation_index in
       let lcode_label =  Printf.sprintf "LClosureCode%d" operation_index in
       
       (* Allocate NewExtEnv *)
-      let allocate_new_extenv_cmd = "EXTEND_ENV(rbx)" in
-      let mov_env_pointer_to_rbx = ";;; EXTEND_ENV puts the address in rax, but we want env pointer to be in rbx\npush rbx\nmov rbx, rax" in
-            
-      (* Address of closure in RAX, correct stack by pop rbx *)
+      let envextend_label = Printf.sprintf "LClosureEnvExtend%d" operation_index in
+      let envextend_end_label = Printf.sprintf "LClosureEnvExtend_End%d" operation_index in
+      let envextend_loop_label =  Printf.sprintf "LClosureEnvExtend_Loop%d" operation_index in
+      let envextend_loop_end_label =  Printf.sprintf "LClosureEnvExtend_LoopEnd%d" operation_index in
+      let envextend_params_loop_label = Printf.sprintf "LClosureEnvExtend_ParamsLoop%d" operation_index in
+      let envextend_params_loop_end_label = Printf.sprintf "LClosureEnvExtend_ParamsLoopEnd%d" operation_index in
+      let bytes_to_allocate = depth * 8 in
+      
+      (* Check Depth Code *)
+      let check_depth_code = Printf.sprintf 
+"; Env Extend Code Section For Closure#%d
+mov rbx, %d ; move the depth (== length of env on stack) to rbx
+cmp rbx, 0
+jne %s
+; if it's equal, we are at the first level
+; so there is no env to extend
+mov rbx, SOB_NIL_ADDRESS
+jmp %s
+" operation_index depth envextend_label envextend_end_label in
+
+      let envextend_label_code = Printf.sprintf
+"%s:
+  ; rbx = the depth == the length of the ext_env
+  
+  ; store registers for personal use
+  push rax
+  push rcx
+  push rdx
+
+  ; store in rcx the length of the env on the stack
+  mov rcx, %d
+  ; rax = nothing
+  ; rbx = the depth == the length of the ext_env
+  ; rcx = length of env on stack
+  ; rdx = nothing
+
+  ; get the address of the current major list (on stack)
+  mov rbx, qword [ENV_STACK_POSITION]
+  ; rax = nothing
+  ; rbx = address of env major list
+  ; rcx = length of env
+  ; rdx = nothing
+
+  ; allocate the ext_env major list
+  MALLOC rax, %d
+  ; rax = address of ext_env major list
+  ; rbx = address of env major list
+  ; rcx = length of env
+  ; rdx = nothing
+" envextend_label (depth - 1) bytes_to_allocate in
+      let envextend_loop_label_code = Printf.sprintf 
+" ; LOOP
+  cmp rcx, 0
+%s:
+  jz %s
+  mov rdx, rcx
+  ; rax = address of ext_env major list
+  ; rbx = address of env major list
+  ; rcx = length of env
+  ; rdx = length of env
+
+  dec rcx
+  ; rax = address of ext_env major list
+  ; rbx = address of env major list
+  ; rcx = length of env - 1
+  ; rdx = length of env
+
+  ; annotate rcx = n, so rdx = n + 1
+  ; need to do: rax + WORD_SIZE * (n + 1) <- rbx + WORD_SIZE * n
+  ; DOING : mov qword [rax + rdx*WORD_SIZE], qword [rbx + rcx*WORD_SIZE]
+  ; store registers for personal use
+  push r8
+  push r9
+
+  lea r8, [rax + rdx*WORD_SIZE]
+  lea r9, [rbx + rcx*WORD_SIZE]
+  mov r9, qword [r9]
+  mov qword [r8], r9
+
+  ; revert registers used for personal use
+  pop r9
+  pop r8
+
+  jmp %s
+" envextend_loop_label envextend_loop_end_label envextend_loop_label in
+      
+      let envextend_loop_end_label_code = Printf.sprintf
+"%s:
+  ; rax = address of ext_env major list
+  ; rbx = address of env major list
+  ; rcx = 0
+  ; rdx = 1
+
+  ; copy value of rax
+  mov rbx, rax
+  ; rax = address of ext_env major list
+  ; rbx = address of ext_env major list
+  ; rcx = 0
+  ; rdx = 1
+
+  ; get the args_count from stack
+  mov rcx, qword [ARGS_COUNT_POSITION]
+  ; rax = address of ext_env major list
+  ; rbx = address of ext_env major list
+  ; rcx = args number
+  ; rdx = 1
+
+  ; check if there are params to copy
+  cmp rcx, 0
+  je %s
+  
+  ; calculate the number of bytes to allocate for extenv[0]
+  mov rax, rcx
+  mov rdx, WORD_SIZE
+  imul rdx
+  ; rax = number of bytes to allocate for extenv[0]
+  ; rbx = address of ext_env major list
+  ; rcx = args number
+  ; rdx = 8
+
+  ; allocate extenv[0] rib
+  MALLOC rax, rax
+  ; rax = address of extenv[0] rib
+  ; rbx = address of ext_env major list
+  ; rcx = args number
+  ; rdx = 8
+
+  ; link address of extenv[0] into ext_env major list in position 0
+  mov qword [rbx], rax
+  ; rax = address of extenv[0] rib
+  ; rbx = address of ext_env major list (now with all ribs linked)
+  ; rcx = args number
+  ; rdx = 8
+" envextend_loop_end_label envextend_params_loop_end_label in
+
+      let envextend_params_loop_label_code = Printf.sprintf 
+"; COPY PARAMS LOOP
+cmp rcx, 0
+%s:
+  jz %s
+
+  dec rcx
+  ; rax = address of extenv[0] rib
+  ; rbx = address of ext_env major list (now with all ribs linked)
+  ; rcx = args number - 1 [first iteration, from second: rcx = rcx - 1]
+  ; rdx = 8
+
+  ; annotate rcx = n
+  ; need to do rax + WORD_SIZE * n <- PVAR(n)
+  push r8
+  mov r8, PVAR(rcx)
+  mov qword [rax + WORD_SIZE*rcx], r8
+  pop r8
+
+  jmp %s
+" envextend_params_loop_label envextend_params_loop_end_label envextend_params_loop_label in
+
+      let envextend_params_loop_end_label_code = Printf.sprintf
+"%s:
+  ; rax = address of extenv[0] rib
+  ; rbx = address of ext_env major list (now with all ribs linked)
+  ; rcx = 0
+  ; rdx = 8
+
+  ; we used these regs at the begining of the extension, so pop them back
+  pop rdx
+  pop rcx
+  pop rax
+%s:
+  ; address is in RBX as expected
+" envextend_params_loop_end_label envextend_end_label in
+
+      let allocate_new_extenv_cmd = 
+        String.concat "\n" 
+      ["push rbx";
+      check_depth_code;
+      envextend_label_code;
+      envextend_loop_label_code;
+      envextend_loop_end_label_code;
+      envextend_params_loop_label_code;
+      envextend_params_loop_end_label_code] in
+
+      let mov_env_pointer_to_rbx = ";;; EXTEND_ENV puts the address in rbx" in
+
+      (* Address of closure in RAX *)
       let allocate_closure = Printf.sprintf "MAKE_CLOSURE(rax, rbx, %s)\npop rbx" lcode_label in
 
       let jump_lcont = Printf.sprintf "jmp %s" lcont_label in
