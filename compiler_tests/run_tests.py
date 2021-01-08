46149082e9ce1b0105a29ca8572ea3a71774032b
sagivbgu <sagivsc@post.bgu.ac.il>
2021-01-08 14:16:31 +0200

    Added tests + Improved run_tests script to show "2nd-order pass"

--- compiler_tests/run_tests.py
+++ compiler_tests/run_tests.py
@@ -3,6 +3,10 @@ import subprocess
 import difflib
 from sys import argv
 
+TEST_PASS = "passed"
+TEST_FAIL = "failed"
+TEST_SECOND_ORDER_PASS = "2nd-order passed"
+
 def make_cmd(fn):
     cmd = "cp inputs/{0}.scm .. && cd .. && make {0} && mv {0} compiler_tests/executables && mv {0}.s compiler_tests/bin && rm {0}*".format(fn)
     return cmd
@@ -14,6 +18,9 @@ def run_compiler(filename):
 
 def run_chez(filename):
     os.system("chezscheme9.5 --quiet < ./inputs/{} >& ./expected_outputs/{}".format(filename, filename[:-4]))
+
+def run_chez_on_compiler_output(filename):
+    os.system("chezscheme9.5 --quiet < ./outputs/{0} >& ./outputs/rerun_{0} && mv ./outputs/rerun_{0} ./outputs/{0}".format(filename[:-4]))
 
 def compare(filename):
     output_name = filename[:-4]
@@ -35,28 +42,45 @@ def print_test_results(report):
         print(test)
         print(report[test].strip() + "\n")
 
+def run_tests(input_files):
+    report = {}
+    for test in input_files:
+        run_compiler(test)
+        run_chez(test)
+        line_diff = list(compare(test))
+        if line_diff == []:
+            report[test] = TEST_PASS
+        else:
+            report[test] = TEST_FAIL + ": \n" + " ".join(line_diff)
+    return report
+
+def rerun_failed_tests(report):
+    for (test, description) in report.items():
+        if description.startswith(TEST_FAIL):
+            run_chez_on_compiler_output(test)
+            line_diff = list(compare(test))
+            if line_diff == []:
+                report[test] = report[test].replace(TEST_FAIL, TEST_SECOND_ORDER_PASS)
+
 if __name__ == "__main__":
     if (len(argv) > 1):
         input_files = ['{}.scm'.format(argv[1])]
     else:
         input_files = os.listdir('./inputs')
-    report = {}
-    tests_total = 0
-    tests_passed = 0
-    for test in input_files:
-        tests_total += 1
-        run_compiler(test)
-        run_chez(test)
-        line_diff = []
-        for line in compare(test):
-            line_diff.append(line)
-        if line_diff == []:
-            report[test] = "passed"
-            tests_passed += 1
-        else:
-            report[test] = "failed: \n" + " ".join(line_diff)
+    
+    
+    report = run_tests(input_files)
+    rerun_failed_tests(report)
+    
+    tests_total = len(input_files)
+    tests_passed = len([(test, description) for (test, description) in report.items() if description.startswith(TEST_PASS)])
+    tests_second_order_passed = len([(test, description) for (test, description) in report.items() if description.startswith(TEST_SECOND_ORDER_PASS)])
+    tests_failed = len([(test, description) for (test, description) in report.items() if description.startswith(TEST_FAIL)])
+
     if tests_passed != tests_total:
-        print("passed {}/{} tests".format(tests_passed,tests_total))
+        print("passed: {}/{} tests".format(tests_passed,tests_total))
+        print("passed (2nd order): {}/{} tests".format(tests_second_order_passed,tests_total))
+        print("failed: {}/{} tests\n".format(tests_failed,tests_total))
         print_test_results(report)
     else:
         print("passed all tests!")
